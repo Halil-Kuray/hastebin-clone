b7440265722688cab6004afa2087761ddc1e180c
Halil-Kuray <71791007+Halil-Kuray@users.noreply.github.com>
2023-03-26 17:05:46 +0200

    ejs file html and cdn links added

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const code = `Welcome to Hastebin Clone
     
 Use the commands in the top right corner
 to create a new file to share with others.`
-    res.render('code-display', {code})
+
+const lineNumbers = code.split('\n').length
+    res.render('code-display', { code, lineNumbers })
 })
 
 
