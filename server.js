9ac528b42b5b87d413c08e655178c881e723f11f
Halil-Kuray <71791007+Halil-Kuray@users.noreply.github.com>
2023-03-26 23:54:23 +0200

    logic applied and is working

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+const { urlencoded } = require("express")
 const express = require("express")
 const app = express()
+const mongoose = require('mongoose')
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+mongoose.connect('mongodb://localhost/hastebin-clone', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
 
 
 
@@ -15,12 +24,19 @@ Use the commands in the top right corner
 to create a new file to share with others.`
 
 const lineNumbers = code.split('\n').length
+
     res.render('code-display', { code, lineNumbers })
 })
 
 
+app.get('/new', (req,res) => {
+    res.render('new')
+})
 
-
+app.post('/save', (req, res) => {
+    const body = req.body.value
+    console.log(body)
+})
 
 
 
