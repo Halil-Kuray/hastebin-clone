32036ce96bc29e803e0c39ba4920d1517007ca8e
Halil-Kuray <71791007+Halil-Kuray@users.noreply.github.com>
2023-03-27 00:23:11 +0200

    model created and tested

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const { urlencoded } = require("express")
 const express = require("express")
 const app = express()
 const mongoose = require('mongoose')
+const Document = require('./model/Document.js')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -23,9 +24,7 @@ const code = `Welcome to Hastebin Clone
 Use the commands in the top right corner
 to create a new file to share with others.`
 
-const lineNumbers = code.split('\n').length
-
-    res.render('code-display', { code, lineNumbers })
+    res.render('code-display', { code })
 })
 
 
@@ -33,13 +32,28 @@ app.get('/new', (req,res) => {
     res.render('new')
 })
 
-app.post('/save', (req, res) => {
-    const body = req.body.value
-    console.log(body)
+app.post('/save', async (req, res) => {
+    const value = req.body.value
+    try{
+        const document = await Document.create({value})
+        res.redirect(`/${document.id}`)
+    }catch(e) {
+        res.render('new', {value})
+    }
+    console.log(value)
 })
 
 
+app.get('/:id', async (req, res) => {
+    const id = req.params.id
 
+    try{
+        const document = await Document.findById(id)
+        res.render('code-display', { code: document.value})
+    }catch(e){
+
+    }
+})
 
 
 
